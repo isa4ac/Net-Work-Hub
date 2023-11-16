f17b11117e99f44f9783c2a3c89e49ce4490b86b
Isaac Van Meter <ivanmeter28@gmail.com>
2023-11-16 13:26:49 -0500

    Modified script to filter json nmap data

--- raspberrypi/scannerApp.py
+++ raspberrypi/scannerApp.py
@@ -1,7 +1,41 @@
+import json
 import xmltodict
 from subprocess import run
 from flask import Flask, jsonify
 app = Flask(__name__)
+
+def parsePortData(port):
+    result = []
+    ports = port['port']
+
+    for portData in ports:
+        result.append(portData)
+
+    return result
+
+def parseHostData(hosts):
+    results = []
+    index = 0
+    for host in hosts:
+        result = dict()
+        result['hostid'] = index
+        result['address'] = host['address']
+        result['hostnames'] = host['hostnames']
+        result['ports'] = parsePortData(host['ports'])
+        results.append(result)
+        index += 1
+
+    return results
+
+def parseNmapData(data):
+    result = dict()
+
+    result['args'] = data['@args']
+    result['startstr'] = data['@startstr']
+    result['host'] = parseHostData(data['host'])
+    result['runstats'] = data['runstats']
+
+    return result
 
 @app.route('/openports')
 def openports_currentrange():
@@ -12,7 +46,7 @@ def openports_currentrange():
     with open('portscan.xml') as raw_xml:
         nmap_scan = xmltodict.parse(raw_xml.read())
 
-    return jsonify(nmap_scan)
+    return parseNmapData(jsonify(nmap_scan))
 
 if __name__ == '__main__':
     app.run()
