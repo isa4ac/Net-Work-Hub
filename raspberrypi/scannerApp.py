81e41ab190d1ac82c60d2669e6f1476bf239b799
evan <149713625+e-an@users.noreply.github.com>
2023-11-20 18:43:02 -0500

    test 5
    filters working, needs adjustments

--- raspberrypi/scannerApp.py
+++ raspberrypi/scannerApp.py
@@ -1,53 +1,48 @@
-import json
 import xmltodict
 from subprocess import run
 from flask import Flask, jsonify
 app = Flask(__name__)
 
-def parsePortData(port):
-    result = []
-    ports = port['port']
-
-    for portData in ports:
-        result.append(portData)
-
-    return result
-
-def parseHostData(hosts):
-    results = []
-    index = 0
-    for host in hosts:
-        result = dict()
-        result['hostid'] = index
-        result['address'] = host['address']
-        result['hostnames'] = host['hostnames']
-        result['ports'] = parsePortData(host['ports'])
-        results.append(result)
-        index += 1
-
-    return results
-
-def parseNmapData(data):
-    result = dict()
-
-    result['args'] = data['@args']
-    result['startstr'] = data['@startstr']
-    result['host'] = parseHostData(data['host'])
-    result['runstats'] = data['runstats']
-
-    return result
-
 @app.route('/openports')
 def openports_currentrange():
     """scan host"""
 
-    run(['nmap', '--open', '-oX', 'portscan.xml', '10.10.10.0/24'])
+    run(['nmap', '--open', '-oX', 'scan.xml', '10.10.10.0/24'])
 
-    with open('portscan.xml') as raw_xml:
+    with open('scan.xml') as raw_xml:
         nmap_scan = xmltodict.parse(raw_xml.read())
-        print(xmltodict.unparse(jsonify(nmap_scan)))
 
-    return parseNmapData(xmltodict.unparse(jsonify(nmap_scan)))
+    # filter
+    filtered_scan = {'hosts': []}
 
-if __name__ == '__main__':
-    app.run()
+    for host in nmap_scan.get('nmaprun', {}).get('host', []):
+        filtered_host = {
+            'address': host.get('address', {}).get('@addr'),
+            'hostnames': host.get('hostnames', {}).get('hostname', {}).get('@name') if host.get('hostnames') else None,
+            'ports': []
+        }
+
+        # check if ports is a list before iterating
+        if isinstance(host.get('ports', {}).get('port'), list):
+            for port in host.get('ports', {}).get('port', []):
+                filtered_port = {
+                    'portid': port.get('@portid'),
+                    'protocol': port.get('@protocol'),
+                    'service': port.get('service', {}).get('@name'),
+                    'state': port.get('state', {}).get('@state')
+                }
+                filtered_host['ports'].append(filtered_port)
+        elif isinstance(host.get('ports', {}).get('port'), dict):
+            # case where 'port' is dict
+            port = host.get('ports', {}).get('port')
+            filtered_port = {
+                'portid': port.get('@portid'),
+                'protocol': port.get('@protocol'),
+                'service': port.get('service', {}).get('@name'),
+                'state': port.get('state', {}).get('@state')
+            }
+            filtered_host['ports'].append(filtered_port)
+
+        filtered_scan['hosts'].append(filtered_host)
+
+    return jsonify(filtered_scan)
